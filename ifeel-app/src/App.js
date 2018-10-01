d336df2a76dd2423de00b7dc3fd59d9a4d15adc9
Grant Garrett-Grossman <grantlycee@gmail.com>
2018-10-01 00:26:40 -0500

    My simple edit

--- ifeel-app/src/App.js
+++ ifeel-app/src/App.js
@@ -13,6 +13,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+			<li>Eukaryotes</li>
+			<li>Archaea</li>
+			<li>Bacteria</li>
+		</ul>
       </div>
     );
   }
